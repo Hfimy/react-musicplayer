be9c8bb3ea593f5532ccaf222ce70231fb3d8f70
Hfimy <780986227@qq.com>
2017-09-16 10:18:19 +0800

    'modify'

--- src/component/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../style/header.less'
-
-export default ()=>(
-    <div className='header'>
-        <img src="/image/logo.png" alt="logo" width='40'/>
-        <h1>Music player build by React</h1>
-    </div>
-)
